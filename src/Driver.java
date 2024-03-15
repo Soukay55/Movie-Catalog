f083f01a872d93c7308dcb514518c4e99310b3b3
Soukay55 <haitamis@hotmail.com>
2024-03-15 02:02:43 -0400

    k

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+
+    public static void main (String[]args)
+    {
+        Movie m = new Movie(2004,"I, Robot",115,"Action","PG-13",7.1,"Alex Proyas","Will Smith","Chi Greenwood","Taylor swift");
+        Movie q = new Movie(2004,"I, Robot",115,"Action","PG-13",7.1,"Alex Proyas","Will Smith","Chi Greenwood","Taylor swift");
+        System.out.println(m);
+
+    }
+}
